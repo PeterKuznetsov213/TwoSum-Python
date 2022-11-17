27d813fe3e9514a2df5aca0518baa3bac2d14a49
PeterKuznetsov213 <peterkuznetsov213@gmail.com>
2022-11-17 20:07:24 +0400

    polish

--- testing/main.py
+++ testing/main.py
@@ -2,12 +2,8 @@ def twoSum(nums,target):
            for i in range(len(nums)):
                x=i+1
                for n in range(len(nums)):
-
                    if nums[i]+nums[x]==target:
-
                         return(i,x)
 
 
-# target=9
-# nums=(2,7,11,15)
 print(twoSum((3,2,4), 6))
